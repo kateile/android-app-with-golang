60b457f6c2ea21667c802431aa1492977edbf587
Sylvanus Kateile <kateile@outlook.com>
2022-07-22 10:26:36 +0300

    provide full path for gomobile bind output

--- scripts/build_aar.ps1
+++ scripts/build_aar.ps1
@@ -1,3 +1,4 @@
+Set-PSDebug -Trace 1
 Write-Host "Building aar started!"
 
 #This is current dirctory
@@ -5,7 +6,18 @@ Set-Location $PSScriptRoot
 
 # Switching to server root
 cd..
-cd server/
+
+$root = Get-Location
+Write-Host "root: $root"
+
+$output =  "$root\client\app\libs"
+Write-Host "output dir: $output"
+
+$file =  "$output\server.aar"
+Write-Host "output file: $file"
+
+cd server
+Write-Host "switched to server dir: $(Get-Location)"
 
 #Installing go and build
 go install golang.org/x/mobile/cmd/gomobile@latest
@@ -14,6 +26,6 @@ go get golang.org/x/mobile/cmd/gobind
 go get golang.org/x/mobile/cmd/gomobile
 gomobile init
 
-gomobile bind -v -o ../client/app/libs/server.aar -target=android  -ldflags="-s -w" ./lib
+gomobile bind -v -o $file -target=android  -ldflags="-s -w" ./lib
 
 Write-Host "Done build aar!"
--- scripts/build_app.ps1
+++ scripts/build_app.ps1
@@ -1,9 +1,13 @@
+Set-PSDebug -Trace 1
 Write-Host "Building app started!"
 
 #This is current dirctory
 Set-Location $PSScriptRoot
 
+# Build aar
 ./build_aar.ps1
+
+Set-Location $PSScriptRoot
 
 # Switching to server root
 cd..
